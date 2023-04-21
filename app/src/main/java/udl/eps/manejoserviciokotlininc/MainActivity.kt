5e7ffa948f508993372d079119e32b8e5acecb76
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-04-21 21:11:58 +0200

    ADDED RECEIVER

--- app/src/main/java/udl/eps/manejoserviciokotlininc/MainActivity.kt
+++ app/src/main/java/udl/eps/manejoserviciokotlininc/MainActivity.kt
@@ -1,6 +1,8 @@
 package udl.eps.manejoserviciokotlininc
 
+import android.content.BroadcastReceiver
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,7 +13,7 @@ import udl.eps.manejoserviciokotlininc.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var receiver: BroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,20 +23,28 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         binding.btnFin.setOnClickListener(this)
         binding.btnInC.setOnClickListener(this)
 
+        receiver = MyReceiver()
+        val filter = IntentFilter()
+        filter.addAction("android.intent.action.HEADSET_PLUG")
+        registerReceiver(receiver, filter)
     }
 
     override fun onClick(src: View) {
-        val intent = Intent(this, MyReceiver::class.java)
+        val intent = Intent(this, ElServicio::class.java)
 
         when(src.id) {
             R.id.btnIn -> {
                 intent.putExtra("type", "train")
                 Toast.makeText(this, "Sound start clicked!", Toast.LENGTH_LONG).show()
+                startService(intent)
+            }
+            R.id.btnFin -> {
+                stopService(intent)
             }
             R.id.btnInC -> {
                 intent.putExtra("type", "song")
+                startService(intent)
             }
         }
-        sendBroadcast(intent)
     }
 }
--- app/src/main/java/udl/eps/manejoserviciokotlininc/MyReceiver.kt
+++ app/src/main/java/udl/eps/manejoserviciokotlininc/MyReceiver.kt
@@ -3,10 +3,31 @@ package udl.eps.manejoserviciokotlininc
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class MyReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
 
+    override fun onReceive(context: Context?, intent: Intent?) {
+        /*val intent1 = Intent(context, ElServicio::class.java)
+        val actionToDO = intent1.getStringExtra("action")
+        if(actionToDO == "stopAll"){
+            Toast.makeText(context, "received to stop all", Toast.LENGTH_LONG).show()
+            context?.stopService(intent1)
+        }else{
+            context?.startService(intent1)
+        }*/
+        val intentService = Intent(context, ElServicio::class.java)
+
+        if (intent != null) {
+            if (intent.getIntExtra("state", 0) == 0) {
+                Toast.makeText(context, "Plug on", Toast.LENGTH_LONG).show()
+                context?.stopService(intentService)
+            } else {
+                intentService.putExtra("type", "song")
+                Toast.makeText(context, "Plug", Toast.LENGTH_LONG).show()
+                context?.startService(intentService)
+            }
+        }
     }
 
 }
